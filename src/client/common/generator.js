27ebfac95ee777e4564e2530fee057c7a19e5576
webfrontzhifei <906831852@qq.com>
2017-05-01 01:28:28 +0800

    'app.vue'

---
+++ src/client/common/generator.js
@@ -0,0 +1,28 @@
+/*
+* @Author: frontendzhifei
+* @Date:   2017-05-01 00:39:23
+* @Last Modified by:   frontendzhifei
+* @Last Modified time: 2017-05-01 00:54:10
+*/
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from 'common/containers/app.vue';
+
+Vue.use(VueRouter);
+
+export default function() {
+
+	const router = new VueRouter({
+		mode: 'history',
+		routes: clientRoutes,
+		scrollBehavior() {
+			return { x:0, y:0 };
+		}
+	});
+
+	new Vue({
+		router,
+		render: h => h(App),
+	}).$mount('app');
+}
